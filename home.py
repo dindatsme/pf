e5b4d9c27f51fd59dc41ad8109f3828a331bb7ca
Meilany Dinda Talitha <trlybei@gmail.com>
2025-05-25 20:22:01 +0700

    Update home.py
    update server max upload size

--- home.py
+++ home.py
@@ -26,6 +26,7 @@ key = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(url, key)
 
+st.set_option('server.maxUploadSize', 200)
 st.set_page_config(layout="wide")
 # CSS Custom
 def inject_css():
